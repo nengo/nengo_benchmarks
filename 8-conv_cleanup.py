7689e2751573229e451d374273d6b54bb7f39bd5
Terry Stewart <tcstewar@uwaterloo.ca>
2015-06-18 12:55:37 -0400

    convolution cleanup benchmark

--- 8-conv_cleanup.py
+++ 8-conv_cleanup.py
@@ -2,10 +2,20 @@ import nengo
 import nengo.spa as spa
 import numpy as np
 
+use_spinnaker = False
+
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-D = 16
+D = 16                  # dimensionality
+mem_tau = 0.1           # memory time constant
+mem_input_scale = 0.5   # input scaling on memory
+test_time = 10.0         # amount of time to test memory for
+test_present_time = 0.1 # amount of time for one test
+answer_offset = 0.025    # reaction time
+
+if use_spinnaker:
+    answer_offset += 0.010  # spinnaker has delays due to passthroughs
 
 model = spa.SPA()
 with model:
@@ -15,15 +25,16 @@ with model:
     model.bound = spa.Buffer(D)
 
     cconv = nengo.networks.CircularConvolution(n_neurons=200,
-                                dimensions=16)
+                                dimensions=D)
 
     nengo.Connection(model.shape.state.output, cconv.A)
     nengo.Connection(model.color.state.output, cconv.B)
 
-    nengo.Connection(cconv.output, model.bound.state.input)
+    nengo.Connection(cconv.output, model.bound.state.input,
+                     transform=mem_input_scale, synapse=mem_tau)
 
     deconv = nengo.networks.CircularConvolution(n_neurons=200,
-                                dimensions=16, invert_b=True)
+                                dimensions=D, invert_b=True)
     deconv.label = 'deconv'
 
     model.query = spa.Buffer(D)
@@ -37,7 +48,7 @@ with model:
                     transform=2)
 
     nengo.Connection(model.bound.state.output, model.bound.state.input,
-                        synapse=0.1)
+                        synapse=mem_tau)
 
 
     vocab = model.get_output_vocab('result')
@@ -53,7 +64,7 @@ with model:
     nengo.Connection(model.cleanup.output, model.clean_result.state.input)
 
 
-    stim_time = 0.05
+    stim_time = mem_tau / mem_input_scale
     def stim_color(t):
         if 0 < t < stim_time:
             return 'BLUE'
@@ -74,7 +85,7 @@ with model:
         if t < stim_time*2:
             return '0'
         else:
-            index = int((t - stim_time) / 0.1)
+            index = int((t - stim_time) / test_present_time)
             return ['BLUE', 'RED', 'CIRCLE', 'SQUARE'][index % 4]
 
     model.input = spa.Input(
@@ -84,16 +95,23 @@ with model:
         )
 
     probe = nengo.Probe(model.clean_result.state.output, synapse=0.02)
+    probe_wm = nengo.Probe(model.bound.state.output, synapse=0.02)
 
-import nengo_spinnaker
-nengo_spinnaker.add_spinnaker_params(model.config)
-for node in model.all_nodes:
-    if node.size_in == 0 and node.size_out > 0 and callable(node.output):
-        model.config[node].function_of_time = True
-sim = nengo_spinnaker.Simulator(model)
+if use_spinnaker:
+    import nengo_spinnaker
+    nengo_spinnaker.add_spinnaker_params(model.config)
+    for node in model.all_nodes:
+        if node.size_in == 0 and node.size_out > 0 and callable(node.output):
+            model.config[node].function_of_time = True
+    sim = nengo_spinnaker.Simulator(model)
+else:
+    sim = nengo.Simulator(model)
 
 
-sim.run(1)
+sim.run(stim_time * 2 + test_time)
+
+if use_spinnaker:
+    sim.close()
 
 vocab = model.get_output_vocab('clean_result')
 vals = [None] * 4
@@ -103,9 +121,29 @@ vals[2] = np.dot(sim.data[probe], vocab.parse('BLUE').v)
 vals[3] = np.dot(sim.data[probe], vocab.parse('RED').v)
 vals = np.array(vals)
 
+vocab_wm = model.get_output_vocab('bound')
+vals_wm = [None] * 2
+vals_wm[0] = np.dot(sim.data[probe_wm], vocab.parse('BLUE*CIRCLE').v)
+vals_wm[1] = np.dot(sim.data[probe_wm], vocab.parse('RED*SQUARE').v)
+vals_wm = np.array(vals_wm)
+
+correct = np.zeros_like(vals)
+for i, t in enumerate(sim.trange()):
+    if t > stim_time * 2 + answer_offset:
+        index = int((t - stim_time * 2 - answer_offset) / test_present_time)
+        correct[index % 4, i] = 1.0
+
+rmse = np.sqrt(np.mean((vals - correct).flatten()**2))
+print('rmse: %g' % rmse)
+
+
 import pylab
+pylab.subplot(2,1,1)
 #pylab.plot(sim.trange(), sim.data[probe])
 pylab.plot(sim.trange(), vals.T)
+pylab.plot(sim.trange(), correct.T)
+pylab.subplot(2,1,2)
+pylab.plot(sim.trange(), vals_wm.T)
 pylab.show()
 
 
