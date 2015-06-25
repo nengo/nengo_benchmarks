0bb85dd24212ae491debbcd337dbfbc563fdb422
Terry Stewart <tcstewar@uwaterloo.ca>
2015-06-24 22:48:34 -0400

    Updated convolution exampl

--- 4-convolution.py
+++ 4-convolution.py
@@ -1,38 +1,58 @@
 """
-Nengo Benchmark Model #4: Circular Convolution
+Nengo Benchmark Model: Circular Convolution
 
 Input: two random D-dimensional vectors
 Output: the circular convolution of the inputs
 
 """
 
-D = 16       # dimensionality
-T = 0.5      # time to run the simulation for
 
+import benchmark
 import nengo
 import nengo.spa as spa
+import numpy as np
 
-model = spa.SPA()
-with model:
-    model.inA = spa.Buffer(D)
-    model.inB = spa.Buffer(D)
+class CircularConvolution(benchmark.Benchmark):
+    def params(self):
+        return dict(
+            D=8,      # dimensionality
+            T=0.5,    # time to run the simulation for
+            SD=8,     # subdimensions
+            pstc=0.01 # synapse filter
+            )
+    def benchmark(self, p, Simulator, rng, plt):
+        model = spa.SPA(seed=p.seed)
+        with model:
+            model.inA = spa.Buffer(p.D, subdimensions=p.SD)
+            model.inB = spa.Buffer(p.D, subdimensions=p.SD)
 
-    model.result = spa.Buffer(D)
+            model.result = spa.Buffer(p.D, subdimensions=p.SD)
 
-    model.cortical = spa.Cortical(spa.Actions('result = inA * inB'))
+            model.cortical = spa.Cortical(spa.Actions('result = inA * inB'),
+                                          synapse=p.pstc)
 
-    model.input = spa.Input(inA='A', inB='B')
+            model.input = spa.Input(inA='A', inB='B')
 
-    probe = nengo.Probe(model.result.state.output, synapse=0.05)
+            probe = nengo.Probe(model.result.state.output, synapse=p.pstc)
 
-    ideal = nengo.Node(model.get_output_vocab('inA').parse('A*B').v)
-    probe_ideal = nengo.Probe(ideal)
+            ideal = nengo.Node(model.get_output_vocab('inA').parse('A*B').v)
+            probe_ideal = nengo.Probe(ideal, synapse=None)
 
-sim = nengo.Simulator(model)
-sim.run(T)
+        sim = Simulator(model)
+        sim.run(p.T)
 
-import pylab
-pylab.plot(sim.trange(), sim.data[probe])
-pylab.plot(sim.trange(), sim.data[probe_ideal])
-pylab.show()
+        ideal = sim.data[probe_ideal]
+        for i in range(3):
+            ideal = nengo.synapses.filt(ideal, nengo.Lowpass(0.05), p.dt)
 
+
+        if plt is not None:
+            plt.plot(sim.trange(), sim.data[probe])
+            plt.plot(sim.trange(), ideal)
+
+
+        rmse = np.sqrt(np.mean(sim.data[probe] - ideal)**2)
+        return dict(rmse=rmse)
+
+if __name__ == '__main__':
+    b = CircularConvolution().run()
