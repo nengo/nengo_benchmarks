e1ce0e046cebfa162a3225100e5f5c6e92965702
Terry Stewart <tcstewar@uwaterloo.ca>
2015-06-26 14:04:36 -0400

    updated spa sequence example

--- 5-sequence.py
+++ 5-sequence.py
@@ -1,39 +1,59 @@
 """
-Nengo Benchmark Model #5: SPA Sequence
+Nengo Benchmark Model: SPA Sequence
 
 Given no input, the model will cycle between cortical states using a
 basal ganglia and thalamus.
-
 """
 
 
-dimensions = 32  # dimensionality of the cortical representation
-action_count = 5 # number of states to transition between
-T = 1.0          # time to run the simulation for
-
+import benchmark
 import numpy as np
-
 import nengo
 import nengo.spa as spa
 
-model = spa.SPA()
-with model:
-    model.state = spa.Memory(dimensions=dimensions)
-    actions = ['dot(state, S%d) --> state=S%d' % (i, (i+1)%action_count)
-               for i in range(action_count)]
-    model.bg = spa.BasalGanglia(actions=spa.Actions(*actions))
-    model.thal = spa.Thalamus(model.bg)
+class SPASequence(benchmark.Benchmark):
+    def params(self):
+        self.default('dimensionality', D=32)
+        self.default('number of actions', n_actions=5)
+        self.default('time to simulator', T=1.0)
 
-    def state_input(t):
-        if t<0.1: return 'S0'
-        else: return '0'
-    model.input = spa.Input(state=state_input)
+    def model(self, p):
+        model = spa.SPA()
+        with model:
+            model.state = spa.Memory(dimensions=p.D)
+            actions = ['dot(state, S%d) --> state=S%d' % (i,(i+1)%p.n_actions)
+                       for i in range(p.n_actions)]
+            model.bg = spa.BasalGanglia(actions=spa.Actions(*actions))
+            model.thal = spa.Thalamus(model.bg)
 
-    p = nengo.Probe(model.thal.actions.output, synapse=0.03)
+            def state_input(t):
+                if t < 0.1: 
+                    return 'S0'
+                else: 
+                    return '0'
+            model.input = spa.Input(state=state_input)
 
-sim = nengo.Simulator(model)
-sim.run(T)
+            self.probe = nengo.Probe(model.thal.actions.output, synapse=0.03)
+        return model
 
-import pylab
-pylab.plot(sim.trange(), sim.data[p])
-pylab.show()
+    def evaluate(self, p, sim, plt):
+        sim.run(p.T)
+        self.record_speed(p.T)
+
+        best = np.argmax(sim.data[self.probe], axis=1)
+        change = np.diff(best)
+        change_points = np.where(change != 0)[0]
+        intervals = np.diff(change_points * p.dt)
+
+        if plt is not None:
+            plt.plot(sim.trange(), sim.data[self.probe])
+            plt.plot(sim.trange()[1:], np.where(change!=0,1,0))
+
+
+
+        return dict(period=np.mean(intervals), period_sd=np.std(intervals))
+
+if __name__ == '__main__':
+    SPASequence().run()
+elif __name__ == '__builtin__':
+    model = SPASequence().make_model()
