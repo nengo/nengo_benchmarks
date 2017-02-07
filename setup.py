283428c7cbbdc32788ccba1b36b4fe19084e62a0
Terry Stewart <tcstewar@uwaterloo.ca>
2017-02-07 02:31:41 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='nengo_benchmarks',
+    packages=['nengo_benchmarks'],
+    version='0.5',
+    author='Terry Stewart',
+    description='Benchmarking models for Nengo',
+    url='https://github.com/tcstewar/nengo_benchmarks',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        ]
+    )
