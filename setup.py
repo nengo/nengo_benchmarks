006766626e57bc120ccba874281680d8cd6a4f27
Trevor Bekolay <tbekolay@gmail.com>
2018-04-10 13:55:27 -0400

    nengo_benchmarks -> nengo-benchmarks

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='nengo_benchmarks',
+    name='nengo-benchmarks',
     packages=['nengo_benchmarks'],
     version='0.5',
     author='Terry Stewart',
     description='Benchmarking models for Nengo',
-    url='https://github.com/tcstewar/nengo_benchmarks',
-    license='GPLv3',
+    url='https://github.com/nengo/nengo-benchmarks',
+    license='See LICENSE.rst',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
         'Topic :: Scientific/Engineering',
-        ]
-    )
+    ]
+)
